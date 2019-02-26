2015b4c332dfc2d9263e6b9429905ffc1b71c835
Chia Yew Ken <chiayewken@gmail.com>
2019-02-26 15:08:51 +0800

    convert_hotpot_to_squad_format_classify_support

--- run_hotpot.py
+++ run_hotpot.py
@@ -17,9 +17,11 @@ def create_para_dict(example_dicts):
         example_dicts = [example_dicts]
     return {"paragraphs": example_dicts}
 
+
 def add_yes_no(string):
     # Allow model to explicitly select yes/no from text (location front, avoid truncation)
     return " ".join(["yes", "no", string])
+
 
 def convert_hotpot_to_squad_format(
     json_dict, gold_paras_only=True, combine_context=True
@@ -27,21 +29,52 @@ def convert_hotpot_to_squad_format(
     new_dict = {"data": []}
     count = 0
     for example in json_dict:
-        context_paras = example["context"]
+        raw_contexts = example["context"]
+
         if gold_paras_only:
             support = {
                 para_title: line_num
                 for para_title, line_num in example["supporting_facts"]
             }
-            context_paras = [lst for lst in example["context"] if lst[0] in support]
+            raw_contexts = [lst for lst in raw_contexts if lst[0] in support]
 
-        contexts = ["".join(lst[1]) for lst in context_paras]
+        contexts = ["".join(lst[1]) for lst in raw_contexts]
         if combine_context:
             contexts = [" ".join(contexts)]
 
         answer = example["answer"]
         for context in contexts:
             context = add_yes_no(context)
+            answer_start = context.index(answer) if answer in context else -1
+
+            new_dict["data"].append(
+                create_para_dict(
+                    create_example_dict(
+                        context=context,
+                        answer_start=answer_start,
+                        answer=answer,
+                        id=str(count),  # SquadExample.__repr__ only accepts type==str
+                        is_impossible=(answer_start == -1),
+                        question=example["question"],
+                    )
+                )
+            )
+            count += 1
+    return new_dict
+
+
+def convert_hotpot_to_squad_format_classify_support(json_dict):
+    # Not training model to find answer, but just to "classify" support-ness
+    new_dict = {"data": []}
+    count = 0
+    for example in json_dict:
+        support = {
+            para_title: line_num for para_title, line_num in example["supporting_facts"]
+        }
+
+        for para_title, para_lines in example["context"]:
+            answer = {True: "yes", False: "no"}[para_title in support]
+            context = add_yes_no(" ".join(para_lines))
             answer_start = context.index(answer) if answer in context else -1
 
             new_dict["data"].append(
